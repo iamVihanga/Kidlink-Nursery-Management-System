ff05acfb61b010e6903cf8972409a31e9e5127c4
iamVihanga <vihangarashansilva@gmail.com>
2025-04-30 11:19:07 +0530

    Add: Production RPC Client URL

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -5,5 +5,5 @@ import { env } from "@/lib/env";
 
 // export const client = hc<AppType>(env.NEXT_PUBLIC_APP_URL as string);
 export const client = hc<AppType>(
-  env.NEXT_PUBLIC_APP_URL || ("http://localhost:3000" as string)
+  env.NEXT_PUBLIC_APP_URL || ("https://kidlink.vercel.app" as string)
 );
