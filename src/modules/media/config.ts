e5255b94919c0f6ef6a436b9f103f0f7f1b0814a
iamVihanga <vihangarashansilva@gmail.com>
2025-05-07 17:05:11 +0530

    Revert: Media Uploader Config

--- src/modules/media/config.ts
+++ src/modules/media/config.ts
@@ -1,24 +1,17 @@
 import * as AWS from "@aws-sdk/client-s3";
 import { env } from "@/lib/env";
 
-const config = {
+export const s3Config = {
   region: env.NEXT_PUBLIC_AWS_REGION,
   bucket: env.NEXT_PUBLIC_AWS_S3_BUCKET,
-  accessKeyId: env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
-  secretAccessKey: env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY
-};
-
-export const s3Config = {
-  region: config.region,
-  bucket: config.bucket,
-  baseUrl: `https://${config.bucket}.s3.${config.region}.amazonaws.com`
+  baseUrl: `https://${env.NEXT_PUBLIC_AWS_S3_BUCKET}.s3.${env.NEXT_PUBLIC_AWS_REGION}.amazonaws.com`
 };
 
 // Singleton S3 client instance
 export const s3Client = new AWS.S3({
   region: s3Config.region,
   credentials: {
-    accessKeyId: config.accessKeyId!,
-    secretAccessKey: config.secretAccessKey!
+    accessKeyId: env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID!,
+    secretAccessKey: env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY!
   }
 });
