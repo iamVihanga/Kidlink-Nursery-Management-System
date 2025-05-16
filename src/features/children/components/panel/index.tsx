13fb25da7ecbb3469069c4bd646dddb5bd0dc011
Methmina Binara <binaraepa@gmail.com>
2025-05-16 12:19:06 +0530

    style: optimize child page UI

--- src/features/children/components/panel/index.tsx
+++ src/features/children/components/panel/index.tsx
@@ -4,11 +4,11 @@ import { TabView } from "./tab-view";
 
 export function Panel() {
   return (
-    <div className="flex flex-1 h-[calc(100vh-200px)]">
-      <div className="w-1/3 pr-4">
+    <div className="flex flex-col lg:flex-row gap-4 h-[calc(100vh-200px)]">
+      <div className="w-full lg:w-[320px] h-full">
         <Overview />
       </div>
-      <div className="w-2/3">
+      <div className="w-full lg:flex-1 h-full">
         <TabView />
       </div>
     </div>
