25a0e7b8fab146b4d01c2161b62657c83e9e97b1
Methmina Binara <binaraepa@gmail.com>
2025-05-16 10:41:12 +0530

    feat: Remove unwanted buttons and optimize links

--- src/features/badges/components/badges-table/columns.tsx
+++ src/features/badges/components/badges-table/columns.tsx
@@ -3,7 +3,6 @@
 import { ColumnDef } from "@tanstack/react-table";
 import Image from "next/image";
 import { CellAction } from "./cell-action";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Badge } from "@/types/schema-types";
 
@@ -12,46 +11,39 @@ export const columns: ColumnDef<Badge>[] = [
     accessorKey: "name",
     header: "Name",
     cell: ({ row }) => {
-      if (row.original.imageUrl) {
-        return (
-          <div className="flex items-center gap-3">
-            <Image
-              alt={`${row.original.name}`}
-              src={row.original.imageUrl}
-              width={50}
-              height={50}
-              className="size-8 rounded-md object-cover"
-            />
-
-            <p>{row.original.name}</p>
+      const content = row.original.imageUrl ? (
+        <div className="flex items-center gap-3">
+          <Image
+            alt={`${row.original.name}`}
+            src={row.original.imageUrl}
+            width={50}
+            height={50}
+            className="size-8 rounded-md object-cover"
+          />
+          <p>{row.original.name}</p>
+        </div>
+      ) : (
+        <div className="flex items-center gap-3">
+          <div className="size-8 rounded-md bg-primary flex items-center justify-center text-sm text-primary-foreground">
+            {row.original?.name?.slice(0, 2)}
           </div>
-        );
-      } else {
-        return (
-          <div className="flex items-center gap-3">
-            <div className="size-8 rounded-md bg-primary flex items-center justify-center text-sm text-primary-foreground">
-              {row.original?.name?.slice(0, 2)}
-            </div>
-            <p>{row.original.name}</p>
-          </div>
-        );
-      }
+          <p>{row.original.name}</p>
+        </div>
+      );
+      
+      return (
+        <Link 
+          href={`/dashboard/badges/${row.original.id}`}
+          className="hover:underline text-primary cursor-pointer"
+        >
+          {content}
+        </Link>
+      );
     }
   },
   {
     accessorKey: "description",
     header: "Description"
-  },
-  {
-    accessorKey: "id",
-    header: "Details",
-    cell: ({ row }) => {
-      return (
-        <Button asChild size={"sm"} variant={"link"}>
-          <Link href={`/dashboard/badges/${row.original.id}`}>View</Link>
-        </Button>
-      );
-    }
   },
   {
     accessorKey: "createdAt",
