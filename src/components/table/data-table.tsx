3e5d8ea47121b4718649079ce831ec04b05f6090
Methmina Binara <binaraepa@gmail.com>
2025-05-30 17:40:52 +0530

    fix: table issue

--- src/components/table/data-table.tsx
+++ src/components/table/data-table.tsx
@@ -120,7 +120,12 @@ export function DataTable<TData, TValue>({
                       data-state={row.getIsSelected() && "selected"}
                     >
                       {row.getVisibleCells().map((cell) => (
-                        <TableCell key={cell.id}>
+                        <TableCell
+                          key={cell.id}
+                          className={
+                            cell.column.id === "content" ? "max-w-[300px]" : ""
+                          }
+                        >
                           {flexRender(
                             cell.column.columnDef.cell,
                             cell.getContext()
--- src/features/feedbacks/components/feedback-table/columns.tsx
+++ src/features/feedbacks/components/feedback-table/columns.tsx
@@ -4,32 +4,36 @@ import { ColumnDef } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ChildFeedback } from "@/types/schema-types";
+import { ExpandableCell } from "@/components/table/expandable-cell";
 
 export const columns: ColumnDef<ChildFeedback>[] = [
-  {
-    accessorKey: "content",
-    header: "Content"
-  },
-  {
-    accessorKey: "rating",
-    header: "Rating"
-  },
-  {
-    accessorKey: "childId",
-    header: "Child",
-    cell: ({ row }) => {
-      return (
-        <Button asChild size={"sm"} variant={"link"}>
-          <Link href={`/dashboard/children/${row.original.childId}`}>
-            View Child
-          </Link>
-        </Button>
-      );
-    }
-  },
-  {
-    accessorKey: "createdAt",
-    header: "Created At",
-    cell: ({ row }) => new Date(row.original.createdAt).toLocaleDateString()
-  }
+	{
+		accessorKey: "content",
+		header: "Content",
+		cell: ({ row }) => {
+			return <ExpandableCell content={row.getValue("content")} maxLength={100} />;
+		}
+	},
+	{
+		accessorKey: "rating",
+		header: "Rating"
+	},
+	{
+		accessorKey: "childId",
+		header: "Child",
+		cell: ({ row }) => {
+			return (
+				<Button asChild size={"sm"} variant={"link"}>
+					<Link href={`/dashboard/children/${row.original.childId}`}>
+						View Child
+					</Link>
+				</Button>
+			);
+		}
+	},
+	{
+		accessorKey: "createdAt",
+		header: "Created At",
+		cell: ({ row }) => new Date(row.original.createdAt).toLocaleDateString()
+	}
 ];
