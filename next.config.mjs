72603c1f4ba643e75405a08e9d50199b66d5b57a
iamVihanga <vihangarashansilva@gmail.com>
2025-06-06 14:15:44 +0530

    Add: COnfig for disable eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 import withPlaiceholder from "@plaiceholder/next";
 
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
+  },
   images: {
     remotePatterns: [
       {
